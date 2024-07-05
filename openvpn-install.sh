9ff2725262f82feba25d343ae3a91bbd3c71ad60
milo_avatier <milom@avatier.com>
2024-07-05 13:33:53 +0100

    generate password using pwgen and add to build-client-full using stdin

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -674,7 +674,7 @@ function installOpenVPN() {
 				apt-get update
 			fi
 			# Ubuntu > 16.04 and Debian > 8 have OpenVPN >= 2.4 without the need of a third party repository.
-			apt-get install -y openvpn iptables openssl wget ca-certificates curl
+			apt-get install -y openvpn iptables openssl wget ca-certificates curl pwgen
 		elif [[ $OS == 'centos' ]]; then
 			yum install -y epel-release
 			yum install -y openvpn iptables openssl wget ca-certificates curl tar 'policycoreutils-python*'
@@ -1070,15 +1070,34 @@ function newClient() {
 		read -rp "Client name: " -e CLIENT
 	done
 
-	echo ""
-	echo "Do you want to protect the configuration file with a password?"
-	echo "(e.g. encrypt the private key with a password)"
-	echo "   1) Add a passwordless client"
-	echo "   2) Use a password for the client"
+	# echo ""
+	# echo "Do you want to protect the configuration file with a password?"
+	# echo "(e.g. encrypt the private key with a password)"
+	# echo "   1) Add a passwordless client"
+	# echo "   2) Use a password for the client"
 
-	until [[ $PASS =~ ^[1-2]$ ]]; do
-		read -rp "Select an option [1-2]: " -e -i 1 PASS
-	done
+	# until [[ $PASS =~ ^[1-2]$ ]]; do
+	# 	read -rp "Select an option [1-2]: " -e -i 1 PASS
+	# done
+
+	# CLIENTEXISTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c -E "/CN=$CLIENT\$")
+	# if [[ $CLIENTEXISTS == '1' ]]; then
+	# 	echo ""
+	# 	echo "The specified client CN was already found in easy-rsa, please choose another name."
+	# 	exit
+	# else
+	# 	cd /etc/openvpn/easy-rsa/ || return
+	# 	case $PASS in
+	# 	1)
+	# 		./easyrsa --batch build-client-full "$CLIENT" nopass
+	# 		;;
+	# 	2)
+	# 		echo "⚠️ You will be asked for the client password below ⚠️"
+	# 		./easyrsa --batch build-client-full "$CLIENT"
+	# 		;;
+	# 	esac
+	# 	echo "Client $CLIENT added."
+	# fi
 
 	CLIENTEXISTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c -E "/CN=$CLIENT\$")
 	if [[ $CLIENTEXISTS == '1' ]]; then
@@ -1086,17 +1105,14 @@ function newClient() {
 		echo "The specified client CN was already found in easy-rsa, please choose another name."
 		exit
 	else
+		# generate user password
+        mkdir "${CLIENTDIR}/${CLIENT}"
+        PW=$(pwgen 15 1)
+        echo "${PW}" > "${CLIENTDIR}/${CLIENT}/pass"
+
 		cd /etc/openvpn/easy-rsa/ || return
-		case $PASS in
-		1)
-			./easyrsa --batch build-client-full "$CLIENT" nopass
-			;;
-		2)
-			echo "⚠️ You will be asked for the client password below ⚠️"
-			./easyrsa --batch build-client-full "$CLIENT"
-			;;
-		esac
-		echo "Client $CLIENT added."
+		echo "${PW}"; echo "${PW}" | ./easyrsa build-client-full "${CLIENT}"
+		echo -e "${G}Client $CLIENT added.${C}"
 	fi
 
 	# Home directory of the user, where the client configuration will be written
